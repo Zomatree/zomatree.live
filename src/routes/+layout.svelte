c0df4cf5245238e22cff9bb787cc7e4f4c65625d
Zomatree <me@zomatree.live>
2023-01-12 14:54:00 +0000

    add title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <svelte:head>
+    <title>Zomatree</title>
     <meta property="og:title" content="Hi, I'm Zomatree">
     <meta property="og:url" content="https://zomatree.live">
 </svelte:head>
